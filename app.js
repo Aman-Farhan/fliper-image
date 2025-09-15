a2c081e179c4519d4eb22818ccf9487ce5ff7441
Aman <amanfarhan2008@gmail.com>
2025-09-15 23:16:49 +0500

    fsg

---
+++ app.js
@@ -0,0 +1,46 @@
+let swiperImage = document.getElementsByClassName("swiperImages");
+let topImageIndex = 0;
+
+// console.log(swiperImage);
+
+// setInterval(function () {
+//     for(i = 0 ; i < swiperImage.length; i++) {
+//         swiperImage[i].style.zIndex = 0;
+//     }
+
+//     swiperImage[topImageIndex].style.zIndex = 10;
+//     topImageIndex++
+
+//     if(topImageIndex >= swiperImage.length) {
+//         topImageIndex = 0 
+//     }
+// }, 3000)
+
+function swapeRight (){
+
+    for(i = 0 ; i < swiperImage.length; i++) {
+        swiperImage[i].style.zIndex = 0;
+    }
+
+    swiperImage[topImageIndex].style.zIndex = 10;
+    topImageIndex++
+
+    if(topImageIndex >= swiperImage.length) {
+        topImageIndex = 0 
+    }
+
+}
+
+
+function swapeleft () {
+ for (let i = 0; i < sliderImages.length; i++) {
+        sliderImages[i].style.zIndex = 0;
+    }
+
+    topImageIndex--
+    if (topImageIndex < 0) {
+        topImageIndex = sliderImages.length-1;
+    }
+    sliderImages[topImageIndex].style.zIndex = 10; // undefined
+    console.log(topImageIndex, sliderImages[topImageIndex])
+}
